ddce2c35b8e2177cd5058692241950325f0424cb
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-05-14 17:49:49 -0400

    added 'add page' modal

--- js/loadElement.js
+++ js/loadElement.js
@@ -3,13 +3,16 @@ function loadElement(tableName){
 	if(tableName=='users'){
 		$('#main-panel-heading').html('Users');
 		$('#main-panel-body').load('parts/users.html');
+		$('#main-panel-add-link').attr('data-target', '#addUserModal');
 	}
 	else if(tableName=='posts'){
 		$('#main-panel-heading').html('Posts');
 		$('#main-panel-body').load('parts/posts.html');
+		$('#main-panel-add-link').attr('data-target', '#addPostModal');
 	}
 	else if(tableName=='pages'){
 		$('#main-panel-heading').html('Pages');
 		$('#main-panel-body').load('parts/pages.html');
+		$('#main-panel-add-link').attr('data-target', '#addPageModal');
 	}
 }
