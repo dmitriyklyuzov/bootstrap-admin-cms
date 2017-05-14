7190d02440a97e70c39270ac30898c53b7240059
Dmitriy Klyuzov <dmitriy.klyuzov@gmail.com>
2017-05-14 01:05:56 -0400

    First commit

---
+++ js/loadElement.js
@@ -0,0 +1,14 @@
+function loadElement(tableName){
+	if(tableName=='users'){
+		$('#main-panel-heading').html('Users');
+		$('#main-panel-body').load('parts/users.html');
+	}
+	else if(tableName=='posts'){
+		$('#main-panel-heading').html('Posts');
+		$('#main-panel-body').load('parts/posts.html');
+	}
+	else if(tableName=='pages'){
+		$('#main-panel-heading').html('Pages');
+		$('#main-panel-body').load('parts/pages.html');
+	}
+}
